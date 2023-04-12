21d723dcde6485020799a5a0196a19dec16c45de
KennedyParks <kennedyjparks@gmail.com>
2023-04-11 21:04:25 -0500

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Kennedy Parks on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
